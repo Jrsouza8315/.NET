9df1952da5ad5754bfc0a5f537a60496f0af0540
Jrsouza8315 <hbrcomercialssa@gmail.com>
2022-03-05 23:36:03 -0300

    exemplos

---
+++ myConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using myConsole.Exemplos;
+
+    namespace myConsole
+    {
+
+        class Program
+        {
+            static void Main ()
+            {
+                var s = new Pilha();
+                s.Empilha(1);
+                s.Empilha(10);
+                s.Empilha(100);
+                Console.WriteLine(s.Desempilha());
+                Console.WriteLine(s.Desempilha());
+                Console.WriteLine(s.Desempilha());
+                Console.WriteLine(s.Desempilha());
+            }
+        }
+
+    }
---
+++ myDemos/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+public class Program
+{
+    static void Adicionar20(int x)
+    {
+        a = a + 20
+    }
+
+    public static void Main()
+    {
+        int a = 2;
+        Adicionar20(a);
+
+        WriteLine($"O Valor da variavel a é {a}");
+    }
+}
